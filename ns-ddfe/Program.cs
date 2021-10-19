5df0ba1da649fcc955883faa5dcc458eab37f1ee
Fernando Konflanz <konflanz.info@gmail.com>
2021-10-19 11:28:08 -0300

    Update 19102021.0
    + Adicionando estrutura inicial

---
+++ ns-ddfe/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading.Tasks;
+using ns_ddfe.src.ddfe.unique;
+using ns_ddfe.src.ddfe.bunch;
+using ns_ddfe.src.ddfe.eventos;
+
+namespace ns_ddfe_core
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            configParceiro.token = "";
+            await manifestarDocumento();
+        }
+
+        static async Task downloadUnico()
+        {
+            var requisicaoDownloadUnico = new DownloadUnico.Body
+            {
+                CNPJInteressado = "",
+                chave = "",
+                apenasComXml = true,
+                comEventos = false,
+                incluirPDF = true,
+            };
+
+            var retornoDownload = await DownloadUnico.sendPostRequest(requisicaoDownloadUnico, @"DDFe/");
+            Console.WriteLine();
+        }
+
+        static async Task downlaodLote()
+        {
+            var requisicaoDownloadLote = new DownloadLote.Body
+            {
+                CNPJInteressado = "",
+                tpAmb = 2,
+                incluirPDF = false,
+                modelo = 55,
+                apenasComXml = true,
+            };
+
+            var retornoDownload = await DownloadLote.sendPostRequest(requisicaoDownloadLote, @"DDFe/", true);
+            Console.WriteLine(retornoDownload);
+        }
+
+        static async Task manifestarDocumento() 
+        {
+            var requisicaoManifestarDocumento = new ManifestarDocumento.Body
+            {
+                chave = "",
+                CNPJInteressado = "",
+                manifestacao = new ManifestarDocumento.Body.Manifestacao
+                {
+                    tpEvento = "",
+                    xJust = "TESTE FAVOR DESCONSIDERAR"
+                }
+
+            };
+
+            var retornoManifestarDocumento = await ManifestarDocumento.sendPostRequest(requisicaoManifestarDocumento);
+            Console.WriteLine(retornoManifestarDocumento);
+        }
+    }
+}
---
+++ ns-ddfe/src/ddfe/manifestarDocumento.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using ns_ddfe.src.commons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ns_ddfe.src.ddfe.eventos
+{
+    public class ManifestarDocumento
+    {
+        public class Body
+        {
+            public string CNPJInteressado { get; set; }
+            public string nsu { get; set; }
+            public string chave { get; set; }
+
+            public Manifestacao manifestacao { get; set; }
+
+            public class Manifestacao
+            {
+                public string tpEvento { get; set; }
+                public string xJust { get; set; }
+            }
+        }
+
+        public class Response
+        {
+            public string status { get; set; }
+            public string motivo { get; set; }
+            public dynamic retEvento { get; set; }
+            public string erro { get; set; }
+
+        }
+
+        public static async Task<Response> sendPostRequest(Body requestBody)
+        {
+            string url = "https://ddfe.ns.eti.br/events/manif";
+
+            var responseAPI = JsonConvert.DeserializeObject<Response>(await nsAPI.postRequest(url, JsonConvert.SerializeObject(requestBody, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore })));
+
+            return responseAPI;
+        }
+    }
+}
